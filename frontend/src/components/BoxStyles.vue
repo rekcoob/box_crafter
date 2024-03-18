5d7eeef120189866480f766d1665bcf3ec71e2a9
rekcoob <John@example.com>
2024-03-18 18:41:01 +0100

    Add Fefco Code to BoxStyles Component: Frontend

--- frontend/src/components/BoxStyles.vue
+++ frontend/src/components/BoxStyles.vue
@@ -27,20 +27,24 @@ export default {
       <option value="half-open">half-open</option>
     </select>
     <div v-if="selectedOption === 'box'">
-      <p>Regular box</p>
+      <p>Krabica</p>
+      <p><b>FEFCO Code:</b> 0200</p>
       <img src="@/assets/images/box.jpg" alt="box image" />
     </div>
     <div v-else-if="selectedOption === 'box-open'">
-      <p>Box With Open Up</p>
+      <p>Klopová krabica</p>
+      <p><b>FEFCO Code:</b> 0201</p>
       <img src="@/assets/images/box-open.jpg" alt="box-open" />
     </div>
     <div v-else-if="selectedOption === 'half'">
-      <p>Half of Box</p>
+      <p>Polovica krabice</p>
+      <p><b>FEFCO Code:</b> 0200</p>
       <img src="@/assets/images/half.jpg" alt="half" />
     </div>
 
     <div v-else-if="selectedOption === 'half-open'">
-      <p>Half For Open Box</p>
+      <p>Polovica klopovej krabice</p>
+      <p><b>FEFCO Code:</b> 0201</p>
       <img src="@/assets/images/half-open.jpg" alt="half-open" />
     </div>
   </div>
