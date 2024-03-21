f52c00c9abc4927c9226b96260c300c0838284eb
rekcoob <John@example.com>
2024-03-21 18:52:57 +0100

    Add CSS to Select Element :FE

--- frontend/src/components/BoxStyles.vue
+++ frontend/src/components/BoxStyles.vue
@@ -20,13 +20,15 @@ export default {
 <template>
   <div class="boxstyles">
     <h3>Typ Krabice</h3>
-    <select v-model="selectedOption" @change="sendSelectedOption">
-      <option value="box">krabica</option>
-      <option value="box-open">krabica otvorená</option>
-      <option value="half">polovička</option>
-      <option value="half-open">polovička otvorená</option>
-      <option value="box-q">test</option>
-    </select>
+    <div class="custom-select-wrapper">
+      <select v-model="selectedOption" @change="sendSelectedOption" class="custom-select">
+        <option value="box">krabica</option>
+        <option value="box-open">krabica otvorená</option>
+        <option value="half">polovička</option>
+        <option value="half-open">polovička otvorená</option>
+        <option value="box-q">test</option>
+      </select>
+    </div>
     <div v-if="selectedOption === 'box'">
       <p>Krabica</p>
       <p><b>FEFCO Code:</b> 0201</p>
@@ -64,5 +66,29 @@ img {
   width: 500px; /* Adjust the width as per your requirement */
   height: auto; /* Maintain aspect ratio */
   /* height: 300px; */
+}
+.custom-select {
+  display: block;
+  width: 100%;
+  padding: 0.375rem 2.25rem 0.375rem 0.75rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  color: inherit;
+  background-color: inherit;
+  border: 1px solid #1e84ff;
+  border-radius: 0.25rem;
+  transition:
+    border-color 0.15s ease-in-out,
+    box-shadow 0.15s ease-in-out;
+}
+.custom-select:focus {
+  border-color: #1e84ff;
+  outline: 1;
+  box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
+}
+/* Dropdown menu */
+.custom-select option {
+  background-color: #333; /* Dark background color */
+  color: #fff; /* Text color */
 }
 </style>
